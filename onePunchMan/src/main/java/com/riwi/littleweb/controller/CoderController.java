8bc789641932a1c71be0b823e011d2567ee67cfd
cristianbyte <charlsmaritz@gmail.com>
2024-04-18 09:34:41 -0500

    actions

--- onePunchMan/src/main/java/com/riwi/littleweb/controller/CoderController.java
+++ onePunchMan/src/main/java/com/riwi/littleweb/controller/CoderController.java
@@ -3,10 +3,15 @@ package com.riwi.littleweb.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.riwi.littleweb.entity.Coder;
 import com.riwi.littleweb.services.CoderService;
@@ -19,19 +24,57 @@ public class CoderController {
     private CoderService objCoderService;
 
     @GetMapping
-    public String showGetAll(Model objModel){
-
-        List<Coder> list = this.objCoderService.findAll();
+    public String showGetAll(Model objModel, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "3") int size){
+        
+        Page<Coder> list = this.objCoderService.findPaginated(page -1 ,size);
+        
         objModel.addAttribute("coderList", list);
+        objModel.addAttribute("currentPage", page);
+        objModel.addAttribute("totalPages", list.getTotalPages());
+
         
         //name HTML view
         return "viewCoder";
     }
 
     @GetMapping("/form")
-    public String showForm(){
-        
+    public String showForm(Model objModel){
+
+        objModel.addAttribute("coder", new Coder());
+        objModel.addAttribute("action", "/coder/create");
         //name HTML view
         return "viewForm";
+    }
+
+    //Update form
+    @GetMapping("/update/{id}")
+    public String showFormUpdate(@PathVariable Long id, Model objModel){
+        Coder obCoderFind = this.objCoderService.findById(id);
+        objModel.addAttribute("coder", obCoderFind);
+        objModel.addAttribute("action", "/edit/"+id);
+        return "viewForm";
+    }
+
+    /*Method for insert a coder by POST */
+    /*@ModelAttribute get view's information */
+    @PostMapping("/coder/create")
+    public String createCoder(@ModelAttribute Coder objCoder){
+        //calling service
+        this.objCoderService.insert(objCoder);
+
+        return "redirect:/";
+    }
+
+
+    @PostMapping("/edit/{id}")
+    public String updateCoder(@PathVariable Long id,@ModelAttribute Coder objCoder){
+        this.objCoderService.update(id, objCoder);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        this.objCoderService.delete(id);
+        return "redirect:/";
     }
 }
--- onePunchMan/src/main/java/com/riwi/littleweb/services/CoderService.java
+++ onePunchMan/src/main/java/com/riwi/littleweb/services/CoderService.java
@@ -3,6 +3,9 @@ package com.riwi.littleweb.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import com.riwi.littleweb.entity.Coder;
@@ -17,4 +20,40 @@ public class CoderService {
     public List<Coder> findAll(){
         return this.objCoderRepo.findAll();
     }
+
+    /* Service to save a coder*/
+    public Coder insert(Coder objCoder) {
+        return this.objCoderRepo.save(objCoder);
+    }
+
+    public Coder update(Long id, Coder objCoder){
+
+        Coder objCoderDB = this.findById(id);
+
+        if(objCoder == null) return null;
+
+        //Validate exist
+        objCoderDB = objCoder;
+        //save
+        return this.objCoderRepo.save(objCoderDB);
+    }
+
+    public Coder findById(Long id){
+        return this.objCoderRepo.findById(id).orElse(null);
+    }
+
+    public void delete(Long id){
+        this.objCoderRepo.deleteById(id);;
+    }
+
+    public <Page>Coder fingPaginated(int page, int size){
+        if(page < 0){
+            page = 1;
+        }
+
+        Pageable objPageable = PageRequest.of(page, size);
+
+        return this.objCoderRepo.findAll(objPageable);
+    }
+
 }
