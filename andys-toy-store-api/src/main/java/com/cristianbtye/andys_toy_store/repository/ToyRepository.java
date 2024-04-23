92d0ecc8207a4dedab14e2a7145d6468014abe1e
cristianbyte <charlsmaritz@gmail.com>
2024-04-23 09:29:59 -0500

    comments

--- andys-toy-store-api/src/main/java/com/cristianbtye/andys_toy_store/repository/ToyRepository.java
+++ andys-toy-store-api/src/main/java/com/cristianbtye/andys_toy_store/repository/ToyRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.cristianbtye.andys_toy_store.entities.Toy;
 
+// Toy <- Entidad | Long <- IDtype
 @Repository
 public interface ToyRepository extends JpaRepository<Toy, Long >{
 
