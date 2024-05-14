0707e35a6fc791da056825c97c388018171df3eb
cristianbyte <charlsmaritz@gmail.com>
2024-05-14 09:22:26 -0500

    update

--- beautyhub-secure/src/main/java/com/riwi/beautySalon/config/SecurityConfig.java
+++ beautyhub-secure/src/main/java/com/riwi/beautySalon/config/SecurityConfig.java
@@ -1,14 +1,32 @@
 package com.riwi.beautySalon.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.riwi.beautySalon.infraestructure.helpers.JwtAuthenticationFilter;
+
+import jakarta.servlet.Filter;
+import lombok.AllArgsConstructor;
+
 
 @Configuration
 @EnableWebSecurity //CONFIG SPRING SECURITY
+@AllArgsConstructor
 public class SecurityConfig {
+
+    @Autowired
+    private final AuthenticationProvider authenticationProvider;
+    @Autowired
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
     // PUBLIC ROUTES
     private final String[] PUBLIC_RESOURCES = {"/services/public/get","/auth/**"};
 
@@ -21,6 +39,11 @@ public class SecurityConfig {
             .authorizeHttpRequests(authRequest -> authRequest
                 .requestMatchers(PUBLIC_RESOURCES).permitAll()
                 .anyRequest().authenticated()
-            ).build();
+            )
+            .sessionManagement(sessionManager -> sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(this.authenticationProvider)
+
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
     }
 }
--- beautyhub-secure/src/main/java/com/riwi/beautySalon/infraestructure/helpers/JwtAuthenticationFilter.java
+++ beautyhub-secure/src/main/java/com/riwi/beautySalon/infraestructure/helpers/JwtAuthenticationFilter.java
@@ -1,28 +1,64 @@
 package com.riwi.beautySalon.infraestructure.helpers;
 
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jersey.JerseyProperties.Filter;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import lombok.var;
 
 @Component
 @AllArgsConstructor
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
     @Autowired
     private final JwtService jwtService;
     @Autowired
     private final UserDetailsService userDetailsService; 
 
-    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response){
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException{
         //get token
         final String token = getTokenFromRequest(request);
 
+        if(token == null){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // get user from token 
+        String userName = this.jwtService.getUsernameFromToken(token);
+
+        // if not find in spring security context
+        if(userName != null && SecurityContextHolder.getContext().getAuthentication() == null ){
+            //get user from database
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
+
+            //if usera and token is valid
+            if(this.jwtService.isTokenValid(token, userDetails)){
+                //create authentication and spring security context
+                var authToken = new UsernamePasswordAuthenticationToken(userName, null,userDetails.getAuthorities());
+                // add authentication details based on request
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                //   
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
     }
 
     public String getTokenFromRequest(HttpServletRequest request){
